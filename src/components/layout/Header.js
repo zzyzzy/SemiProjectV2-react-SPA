294b1130a6fa939944acabe3225560f7fc493c20
JaeSeong.Song <zzyzigy@gmail.com>
2023-03-07 17:37:49 +0900

    a 태그를 navlink으로 전환

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,15 +1,16 @@
 import React, {Fragment} from "react";
+import {NavLink} from "react-router-dom";
 const Header = () => {
     return (
         <>
-            <header><h1>JSP 프로젝트 v1</h1></header>
+            <header><h1>React 프로젝트 v1</h1></header>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/member/join">회원가입</a></li>
-                    <li><a href="/member/login">로그인</a></li>
-                    <li><a href="/board/list">게시판</a></li>
-                    <li><a href="/member/myinfo">회원정보</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/member/join">회원가입</NavLink></li>
+                    <li><NavLink to="/member/login">로그인</NavLink></li>
+                    <li><NavLink to="/board/list">게시판</NavLink></li>
+                    <li><NavLink to="/member/myinfo">회원정보</NavLink></li>
                 </ul>
                 <hr />
             </nav>
